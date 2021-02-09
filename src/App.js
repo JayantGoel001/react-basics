4e4ff058785e95d30b4ac5e902bd2790a2b4e4ec
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-10 03:04:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {Route,Link,Switch} from 'react-router-dom'
 
 class App extends React.Component{
     render() {
         return (
-            <div>
-                <div>
-                    <h2>React Router Demo</h2>
-                    <nav className="navbar navbar-default">
-                        <ul className="nav navbar-nav">
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/contact">Contact</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <hr/>
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/about" component={Home}/>
-                        <Route path="/contact" component={Home}/>
-                        <Route component={NotFound}/>
-                    </Switch>
-                </div>
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
-                    </header>
-                </div>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
             </div>
         );
     }
---
+++ src/nav.js
@@ -0,0 +1,35 @@
+import {Link, Route, Switch} from "react-router-dom";
+import About from "./about";
+import Contact from "./contact";
+import NotFound from "./NotFound";
+import App from "./App";
+
+class Nav {
+    render() {
+        return(
+        <div>
+            <h2>React Router Demo</h2>
+            <nav className="navbar navbar-default">
+                <ul className="nav navbar-nav">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <hr/>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </div>
+        )
+    }
+}
