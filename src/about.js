c30b68c49f98cc3f9acb9a03161cb9b6c0388679
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-10 03:01:12 +0530

    Initial Commit

---
+++ src/about.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div style={{textAlign:'center'}}>
+                <h1>About Page</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
