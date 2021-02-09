e07f81c69594211a1b15e80f540b811150845fbe
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-10 03:17:29 +0530

    Initial Commit

--- src/nav.js
+++ src/nav.js
@@ -1,35 +1,37 @@
-import {Link, Route, Switch} from "react-router-dom";
+import {Router, Link, Route, Switch} from "react-router-dom";
 import About from "./about";
 import Contact from "./contact";
 import NotFound from "./NotFound";
 import App from "./App";
-import * as React from "react";
+import React from "react";
 
 class Nav extends React.Component{
     render() {
         return(
         <div>
-            <h2>React Router Demo</h2>
             <nav className="navbar navbar-default">
-                <ul className="nav navbar-nav">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
+                <Router history="">
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                    <hr/>
+
+                    <Switch>
+                        <Route exact path="/" component={App}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </Router>
             </nav>
-            <hr/>
-            <Switch>
-                <Route exact path="/" component={App}/>
-                <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
-                <Route component={NotFound}/>
-            </Switch>
         </div>
         );
     }
