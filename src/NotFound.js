e2285785062690c1e1015bb53e07870aa9a02927
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-10 03:00:47 +0530

    Initial Commit

---
+++ src/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFound = () => <h1>Page Not Found</h1>
+
+export default NotFound;
---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div style={{textAlign:'center'}}>
+                <h1>Contact Page</h1>
+            </div>
+        );
+    }
+}
+
+export default Contact;
